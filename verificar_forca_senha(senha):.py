63129b852e0ac85f5672a1d253a176c5d39f1511
Jardielen Chaves Sousa <134148247+chaves08r@users.noreply.github.com>
2024-04-04 13:32:18 -0300

    Verificador de senha
    Este projeto consiste em um programa Python que verifica a força das senhas dos usuários com base em critérios de segurança predefinidos. Ele valida se a senha possui no mínimo 8 caracteres, contém pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial. Além disso, o programa verifica se a senha não contém sequências ou palavras comuns. Após a entrada da senha pelo usuário, o programa fornece um feedback indicando se a senha atende aos critérios de segurança e sua força. Este projeto é útil para garantir a segurança dos dados da empresa e incentivar a adoção de senhas mais robustas pelos usuários.

---
+++ verificar_forca_senha(senha):.py
@@ -0,0 +1,50 @@
+def verificar_forca_senha(senha):
+    comprimento_minimo = 8
+    tem_letra_maiuscula = False
+    tem_letra_minuscula = False
+    tem_numero = False
+    tem_caractere_especial = False
+
+    # Verificando o comprimento da senha
+    if len(senha) < comprimento_minimo:
+        return f"Sua senha e muito curta. Recomenda-se no minimo {comprimento_minimo} caracteres."
+
+    # Verificando se a senha contém letras maiúsculas e minúsculas, números e caracteres especiais
+    for caractere in senha:
+        if caractere.isupper():
+            tem_letra_maiuscula = True
+        elif caractere.islower():
+            tem_letra_minuscula = True
+        elif caractere.isdigit():
+            tem_numero = True
+        else:
+            tem_caractere_especial = True
+
+    if (tem_letra_minuscula and tem_numero and tem_caractere_especial):
+      return "Sua senha atende aos requisitos de seguranca. Parabens!"
+
+    # Verificando se todos os critérios foram atendidos
+    if not (tem_letra_maiuscula and tem_letra_minuscula and tem_numero and tem_caractere_especial):
+        return "Sua senha nao atende aos requisitos de seguranca."
+
+    # Verificando se a senha contém sequências comuns
+    sequencias_comuns = ["123456", "abcdef"]
+    for sequencia in sequencias_comuns:
+        if sequencia in senha:
+            return "Sua senha contem uma sequencia comum. Tente uma senha mais complexa."
+
+    # Verificando se a senha contém palavras comuns
+    palavras_comuns = ["password", "123456", "qwerty"]
+    if senha in palavras_comuns:
+        return "Sua senha e muito comum. Tente uma senha mais complexa."
+
+
+
+# Obtendo a senha do usuário
+senha = input().strip()
+
+# Verificando a força da senha
+resultado = verificar_forca_senha(senha)
+
+# Imprimindo o resultado
+print(resultado)
